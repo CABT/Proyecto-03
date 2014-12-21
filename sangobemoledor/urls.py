54d80b1778507a0645549da0eb521b8dd397a5c4
andreagonz <andreagonz@ciencias.unam.mx>
2014-12-20 19:15:00 -0600

    uhhff

---
+++ sangobemoledor/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'sangobemoledor.views.index', name='inicio'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)

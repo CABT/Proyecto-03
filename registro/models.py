5f7d24e87dfc0759e1436c48b6e8af209f2d5c02
andreagonz <andreagonz@ciencias.unam.mx>
2014-12-21 16:00:02 -0600

    porque carlos lo pidio

--- registro/models.py
+++ registro/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django_countries.fields import CountryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Usuario(models.Model):
+
+    usuario = models.OneToOneField(User)
+    nombre = models.CharField(max_length=50)
+    pais = CountryField()
+    imagen = models.ImageField(upload_to='profile_images', blank=True)
+
+    def __unicode__(self):
+        return self.user.nombre

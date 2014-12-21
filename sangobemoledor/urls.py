e11ad1aab09d3dc5c4013f3f67e0c5fb8960a54d
Pernath <cgah.95@gmail.com>
2014-12-20 18:41:40 -0600

    merge

--- sangobemoledor/urls.py
+++ sangobemoledor/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, include, url from django.contrib 
+import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'sangobemoledor.views.home', name='home'),
+    # url(r'^$', 'sangobemoledor.views', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    # No olvidar que esto debe sobrevivir al merge
 
     url(r'^admin/', include(admin.site.urls)),
 )

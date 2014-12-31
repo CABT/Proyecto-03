cc2705606e8acc6e2e32579cbd24ac5d3b485372
Pernath <cgah.95@gmail.com>
2014-12-31 17:30:02 -0600

    perdon

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('.views',
-                url(r'^(?P<usuario>.+)/$',VistaPerfilPublico.as_view(), name='perfil'),
+                url(r'^perfil/(?P<usuario>.+)/$',VistaPerfilPublico.as_view(), name='perfil'),
+                url(r'^error/usuario_inexistente/$',UsuarioInexistente.as_view(), name='error'),
 )

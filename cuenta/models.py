206d4766d33bfde220b57ca7f03b0c480320be17
Pernath <cgah.95@gmail.com>
2014-12-31 04:09:07 -0600

    ge

--- cuenta/models.py
+++ cuenta/models.py
@@ -6,10 +6,10 @@ class Perfil(models.Model):
 	usuario = models.ForeignKey(RegistroUsuario)
 
 	def user(self):
-		return self.RegistroUsuario.username 
+		return self.RegistroUsuario
 
 	def __str__(self):
-		return self.RegistroUsuario.username
+		return self.RegistroUsuario.pk
 
 	def __unicode__(self):
-		return self.RegistroUsuario.username
+		return self.RegistroUsuario.pk

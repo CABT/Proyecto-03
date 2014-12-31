bac4b042df3d32b25770ed077dc3a5df9befd794
Pernath <cgah.95@gmail.com>
2014-12-31 01:36:13 -0600

    por horas

---
+++ cuenta/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from registro.models import RegistroUsuario
+
+# Create your models here.
+class Perfil(models.Model):
+	usuario = models.ForeignKey(RegistroUsuario)
+
+	def __str__(self):
+		return self.RegistroUsuario.username
+
+	def __unicode__(self):
+		return self.RegistroUsuario.username
---
+++ cuenta/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, View, TemplateView
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+
+from .models import Perfil
+# Create your views here.
+
+#Para esta clase utilizare la url /cuenta/usuario
+class VistaPerfilPublico(CreateView):
+	#Aqui obtenemos el usuario, mediante el url
+	def dispatch(self, *args, **kwargs):
+	    self.usuario = self.kwargs['usuario']
+	    return super(VistaPerfil, self).dispatch(*args, **kwargs)
+	def get(self, request, *args, **kwargs):
+		try:
+
+
+#Aqui repetiré basicamente lo de la clase anterior, pero se agregará el metodo de edicion bajo un url /cuenta/codigousuarioactivacion
+
+

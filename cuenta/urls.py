639ae3392d238458141a59268dc4a33b5f691839
andreagonz <andreagonz@ciencias.unam.mx>
2015-01-01 01:11:00 -0600

    dfsdf

--- cuenta/urls.py
+++ cuenta/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 from .views import *
 
 urlpatterns = patterns('.views',
-				url(r'^perfil/(?P<usuario>.+)/$',VistaPerfilPublico.as_view(), name='perfil'),
-                url(r'^error/usuario_inexistente/$',UsuarioInexistente.as_view(), name='error'),
+                       url(r'^perfil/(?P<usuario>.+)/$',VistaPerfilPublico.as_view(), name='perfil'),
+                       url(r'^error/usuario_inexistente/$',UsuarioInexistente.as_view(), name='error'),
 )
--- foro/models.py
+++ foro/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+class EntryQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(publish=True)
+        
 class Categoria(models.Model):
 
     titulo = models.CharField(max_length=60)
@@ -21,7 +25,9 @@ class Hilo(models.Model):
     contenido = models.TextField(max_length=2000)
     fecha = models.DateTimeField(auto_now_add=True)
     publica = models.BooleanField(default=True)
-
+    objects = EntryQuerySet.as_manager()
+    slug = models.SlugField(max_length=200, unique=True)
+    
     def __str__(self):
         return self.titulo
         
@@ -30,18 +36,22 @@ class Hilo(models.Model):
 
     class Meta:
         verbose_name_plural = "Hilos"
+        ordering = ["-created"]
     
 class Comentario(models.Model):
 
     hilo = models.ForeignKey(Hilo)
     contenido = models.TextField(max_length=2000)
+    objects = EntryQuerySet.as_manager()
 
     class Meta:
         verbose_name_plural = "Comentarios"
+        ordering = ["-created"]
         
 class DenunciaComentario(models.Model):
     comentario = models.ForeignKey(Comentario)
-
+    fecha = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.comentario
         
@@ -51,11 +61,12 @@ class DenunciaComentario(models.Model):
     class Meta:
         verbose_name = "Comentario denunciado"
         verbose_name_plural = "Comentarios denunciados"
-        
+        ordering = ["-created"]
         
 class DenunciaHilo(models.Model):
     hilo = models.ForeignKey(Hilo)
-
+    fecha = models.DateTimeField(auto_now_add=True)
+    
     def __str__(self):
         return self.hilo
         
@@ -65,3 +76,4 @@ class DenunciaHilo(models.Model):
     class Meta:
         verbose_name = "Hilo denunciado"
         verbose_name_plural = "Hilos denunciados"
+        ordering = ["-created"]
